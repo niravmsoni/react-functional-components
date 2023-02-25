f37ef7441f7a883c649eda7d53a75f9fb679dbe3
Nirav Soni <soninirav910@gmail.com>
2023-02-25 16:04:03 +0530

    separate state for title and counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,21 +5,23 @@ const Counter = () => {
         return {counter: 10, title:"Fun" }
     });
 
+    const [titleState, setTitleState] = useState("Fun");
+
     function incrementCounter(){
         setCounterState((prevState) => {
-            return {...prevState, counter: prevState.counter + 1};
+            return {counter: prevState.counter + 1};
         });
     }
 
     function decrementCounter(){
         setCounterState((prevState) => {
-            return {...prevState, counter: prevState.counter - 1};
+            return {counter: prevState.counter - 1};
         });
     }
 
     return (
         <div className="col-12 col-md-2 offset-md-5 border  text-white">
-            <span className="h2 pt-4 m-2 text-white-50">{counterState.title} Counter</span>
+            <span className="h2 pt-4 m-2 text-white-50">{titleState} Counter</span>
             <br/>
             <button className="btn btn-success m-1" onClick={incrementCounter}>+1</button>
             <button className="btn btn-danger m-1" onClick={decrementCounter}>-1</button>
