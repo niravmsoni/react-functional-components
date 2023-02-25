44569fbe1ba84e165a39125c0c4384d89e5cb8ba
Nirav Soni <soninirav910@gmail.com>
2023-02-25 14:52:22 +0530

    UI for counter

---
+++ src/Counter.jsx
@@ -0,0 +1,16 @@
+const Counter = () => {
+    return (
+        <div className="col-12 col-md-2 offset-md-5 border  text-white">
+            <span className="h2 pt-4 m-2 text-white-50">Fun Counter</span>
+            <br/>
+            <button className="btn btn-success m-1">+1</button>
+            <button className="btn btn-danger m-1">-1</button>
+            <span className="h4">
+                Counter: &nbsp;
+                <span className="text-success">0</span>
+            </span>
+        </div>
+    );
+};
+
+export default Counter;
